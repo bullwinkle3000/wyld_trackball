d5f8a2e5f095207ca50c998792b52c853a616980
bullwinkle3000 <91431084+bullwinkle3000@users.noreply.github.com>
2022-08-04 12:06:16 -0400

    Generally close

--- trackball.py
+++ trackball.py
@@ -1,14 +1,93 @@
 from jupyter_cadquery.viewer.client import show_object
+
 from cq_shortcuts import *
-from params import *
+
+cutout_margin = 0.5
+size = (10 - cutout_margin * 2, 10 - cutout_margin * 2)
+cutter_radius = 3.175/2
+
+# Ball data
+ball_padding = 2
+wall_thickness = 3.5
+ball_diameter = 34
+ball_radius = ball_diameter / 2
+
+PI2 = math.pi * 2
+PI3 = PI2 / 3
+PI6 = PI3 / 2
+
+# socket
+skt_clearance = 1.5
+skt_wall_thick = 3.5
+
+_skt_hole_dia = ball_diameter + (skt_clearance * 2)
+_skt_dia = _skt_hole_dia + (skt_wall_thick * 2)
+
+padded_ball_radius = ball_radius + ball_padding
+socket_radius = ball_radius + ball_padding + wall_thickness
+
+# Sensor mount data
+sm_screw_dist = 24
+sm_screw_dia_top = 3.1
+sm_screw_dia_btm = 2.8
+sm_screw_mgn = 1.1
+sm_screw_dpth = 8
+sm_screw_cap_dia = sm_screw_mgn + sm_screw_dia_btm
+
+sm_base_h = 21
+sm_base_l = 28.25
+sm_skt_dia = 9
+sm_base_d = 1.5
+sm_skt_z_offset = 2.6
+sm_rot_z = 0
+
+sm_lens_l = 21.4
+sm_lens_w = 19
+sm_lens_d = 3.5
+
+_sm_base_w = sm_screw_dist + ((sm_screw_dia_top + sm_screw_mgn) * 2)
+sm_lens_x = (_sm_base_w - sm_lens_l) / 2
+sm_lens_y = (sm_base_h - sm_lens_w) / 2
+sm_lens_z = 5.2
+
+sm_offset_z = -18.5
+
+## BTU
+_skt_hole_dia = ball_diameter + (skt_clearance * 2)
+
+btu_color = "white"  ## The color of rendered BTUs
+btu_hle_tol = 0.25  ## The tolerance around the BTU for the holes
+btu_base_dia = 12.7  ## The diameter of the stem of the BTU
+btu_base_dpth = 8  ## The depth of the stem of the BTU
+btu_head_dia = 14.5  ## The diameter of the head portion of the BTU
+btu_head_dpth = 1  ## The depth of the head portion of the BTU
+btu_ball_dia = 8.4  ## The diameter of the ball in the BTU
+btu_ball_z_offset = -0.2  ## The z-offset of the BTU ball off the head
+btu_z_mult = 1.3
+
+btu_count = 3
+btu_tilt = 61
+btu_ring_z = 6.3
+
+_btu_base_dia = btu_base_dia + (btu_hle_tol * 2)
+_btu_base_dpth = btu_base_dpth + btu_hle_tol
+
+_btu_head_dia = btu_head_dia + (btu_hle_tol * 2)
+_btu_head_dpth = btu_head_dpth + btu_hle_tol
+_btu_ball_z_offset = btu_ball_dia * btu_ball_z_offset
+_btu_head_top = _btu_base_dpth + _btu_head_dpth
+_btu_ball_height = (btu_ball_dia / 2) - _btu_ball_z_offset
+
+# btu_ring_r = (_skt_hole_dia / 2) + (_btu_ball_height - 1) / 2
+btu_ring_r = 19
 
 
 def screw_hole():
-    return wp().cylinder(sm_screw_depth, sm_screw_dia_btm / 2)
+    return wp().cylinder(sm_screw_dpth, sm_screw_dia_btm / 2)
 
 
 def sensor_mount():
-    sm_base = wp().box(sm_base_w, sm_base_h, sm_base_d).rotate((0, 0, 0), (0, 0, 1), 90).edges("|Z").fillet(8.0)
+    sm_base = wp().box(_sm_base_w, sm_base_h, sm_base_d).rotate((0, 0, 0), (0, 0, 1), 90).edges("|Z").fillet(8.0)
     bottom_hole = wp().box(4, 4, 6).translate((0, -2, 0)).union(wp().cylinder(6, 2))
 
     result = sm_base.cut(bottom_hole).translate((0, 0, sm_offset_z))
@@ -16,38 +95,52 @@ def sensor_mount():
     return result
 
 
-def btu(bearings=False):
-
+def btu():
     def btu_base():
-        return wp().cylinder(btu_base_depth_calc, btu_base_dia_calc / 2)
+        return wp().cylinder(_btu_base_dpth, _btu_base_dia / 2)
 
     def btu_head():
-        return wp().cylinder(btu_base_depth_calc + btu_ball_height_calc, btu_head_dia_calc / 2)
+        return wp().cylinder(_btu_base_dpth + _btu_ball_height, _btu_head_dia / 2)
 
     def btu_ball():
         return wp().sphere(btu_ball_dia / 2)
 
-    b = btu_base()\
-        .union(btu_head().translate((0, 0, btu_base_depth_calc)))\
-        .union(btu_ball().translate((0, 0, (btu_ball_z_offset_calc + btu_head_depth_calc) * -1)))
-
-    if bearings:
-        b.cut(bearing_cutter())
-
-    return b
+    return btu_base()\
+        .union(btu_head().translate((0, 0, _btu_base_dpth)))\
+        .union(btu_ball().translate((0, 0, (_btu_ball_z_offset + _btu_head_dpth) * -1)))
 
 
-def btus(bearings=False):
+btu_z_offset = -10.9
+
+
+def btu_mounts():
     result = None
+
     for i in range(3):
         a = i * PI3
-        b = btu(bearings)
+        b = wp().cylinder(4, 8.5)
+        b = b.cut(wp().cylinder(6, 5))
+        b = rotate_around_z(b, 90)
         b = rotate_around_x(b, btu_tilt)
         b = rotate_around_z(b, math.degrees(-a))
-        x = btu_ring_r * math.sin(a)
-        y = btu_ring_r * math.cos(a)
-        b = b.translate((x, y, -10.9))
+        x = (btu_ring_r - 2) * math.sin(a)
+        y = (btu_ring_r - 2) * math.cos(a)
+        b = b.translate((x, y, btu_z_offset + 1))
+        result = result.union(b) if result is not None else b
 
+    return result
+
+
+def btus():
+    result = None
+
+    for i in range(3):
+        a = i * PI3
+        b = rotate_around_x(btu(), btu_tilt)
+        b = rotate_around_z(b, math.degrees(-a))
+        x = (btu_ring_r + 0.5) * math.sin(a)
+        y = (btu_ring_r + 0.5) * math.cos(a)
+        b = b.translate((x, y, btu_z_offset))
         result = result.union(b) if result is not None else b
 
     return result
@@ -57,12 +150,11 @@ def top_plate():
     tp_thickness = 5
     tp_rim_size = 1.5
     tp_clearance = 0.6
-    _tp_hole_r_top = math.exp(math.log(math.exp(math.log(ball_diameter / 2) * 2) - 
-                                       math.exp(math.log(tp_thickness) * 2)) * 0.5) + tp_clearance
+    _tp_hole_r_top = math.exp(math.log(math.exp(math.log(ball_diameter / 2) * 2) - math.exp(math.log(tp_thickness) * 2)) * 0.5) + tp_clearance
     _tp_hole_r_btn = (ball_diameter / 2) + (tp_clearance * 2)
 
-    result = wp().cylinder(tp_thickness, skt_dia / 2)\
-        .union(wp().cylinder(tp_rim_size, (skt_dia + tp_rim_size + 1) / 2).translate((0, 0, 3.5)))
+    result = wp().cylinder(tp_thickness, _skt_dia / 2)\
+        .union(wp().cylinder(tp_rim_size, (_skt_dia + tp_rim_size + 1) / 2).translate((0, 0, 3.5)))
     result = result.cut(wp().cylinder(tp_thickness + 1, _tp_hole_r_top / 2).translate((0, 0, -0.5)))
     return result
 
@@ -87,7 +179,6 @@ def fin(width, length, corner_radius,):
 #     plane = plane.translate((0, 0, -width/2))
 #     return plane
 
-
 def bearing_cutter():
     cutter = wp().cylinder(3.3, 4).translate((1.7, 0, 0))
     axle_groove = wp().box(4, 3.1, 8).edges(">X").fillet(0.8).translate((1.2, 0, 0))
@@ -100,8 +191,6 @@ def bearing_fin(with_cut=False):
     base_fin = fin(13, 9, axle_radius)
 
     if with_cut:
-        # cutter = wp().cylinder(3.3, 4).translate((1.7, 0, 0))
-        # axle_groove = wp().box(4, 3.1, 8).edges(">X").fillet(0.8).translate((1.2, 0, 0))
         base_fin = base_fin.cut(bearing_cutter())
 
     return base_fin
@@ -151,14 +240,15 @@ def flanges():
 
 def generate_base_socket():
     ball = wp().sphere(padded_ball_radius)
-    bottom_cutter = wp().box(sm_base_w * 2, sm_base_h * 2, sm_base_d * 3).rotate((0, 0, 0), (0, 0, 1), math.pi / 2) \
+    bottom_cutter = wp().box(_sm_base_w * 2, sm_base_h * 2, sm_base_d * 3).rotate((0, 0, 0), (0, 0, 1), math.pi / 2) \
         .translate((0, 0, -(19.8 + (sm_base_d / 2))))
-    box_cutter = wp().box(socket_radius * 2 + ball_padding, socket_radius * 2 + ball_padding, socket_radius * 2) \
+    box_cutter = wp().box(socket_radius * 2 + ball_padding * 4, socket_radius * 2 + ball_padding * 4, socket_radius * 2) \
         .translate((0, 0, socket_radius)).union(ball)
 
-    inner_cyl = wp().cylinder(5, padded_ball_radius)
+    inner_cyl = wp().cylinder(5, ball_radius)
     top_cyl = wp().cylinder(5, socket_radius).cut(inner_cyl).translate((0, 0, 2.5))
-    lip = wp().cylinder(1.25, socket_radius + 1.5).cut(cq.Workplane("XY").cylinder(1.25, ball_radius + 0.1)).translate(
+    # lip = wp().cylinder(1.5, socket_radius + 1.5).cut(cq.Workplane("XY").cylinder(2, ball_radius + 0.2)).translate((0, 0, 5.5))
+    lip = wp().cylinder(1.25, socket_radius + 1.5).cut(cq.Workplane("XY").cylinder(1.25, ball_radius + 0.3)).translate(
         (0, 0, 5.5))
     lip = lip.edges("<Z").chamfer(1.0)
     top_cyl = top_cyl.union(lip)
@@ -186,8 +276,8 @@ def generate_base_socket():
 def generate_btu_socket():
 
     socket = generate_base_socket()
+    socket = socket.union(btu_mounts())
     socket = socket.cut(btus())
-
     socket = rotate_around_z(socket, -90)
 
     return socket
@@ -207,4 +297,4 @@ def generate_bearing_socket():
 
 
 # Render the solid
-show_object(generate_bearing_socket())
+show_object(generate_btu_socket())
