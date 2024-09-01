e192f465b77bdac655d4fce87ccdf4b22b78dfef
bullwinkle3000 <91431084+bullwinkle3000@users.noreply.github.com>
2024-09-01 11:08:10 -0400

    beta 1

--- cq_play.py
+++ cq_play.py
@@ -2,10 +2,10 @@ from cq_shortcuts import *
 from math import sin, cos, pi
 from build123d import *
 from bd_warehouse.thread import IsoThread
-# from ocp_vscode import *
-# from ocp_vscode import show, show_object, reset_show, set_port, set_defaults, get_defaults
+from ocp_vscode import *
+from ocp_vscode import show, show_object, reset_show, set_port, set_defaults, get_defaults
 
-# set_port(3939)
+set_port(3939)
 
 SEGMENTS = 48
 
@@ -381,7 +381,7 @@ def hand_chair():
     tw2 = top_width / 2
     top_height = 10
 
-    side_guard_width = top_width * 2
+    side_guard_width = top_width * 2 + 15
     side_guard_length = 50
     side_guard_height = top_height
 
@@ -405,12 +405,12 @@ def hand_chair():
         return main
 
     def make_guard(sg_width, sg_length, sg_height):
-        rotation = 20
+        rotation = 25
 
-        sg_width_2 = sg_width / 2 + 3
+        sg_width_2 = sg_width / 2
 
-        side_offset = 15
-        guard_right = wp().box(sg_width_2, sg_length, sg_height).translate((sg_width / 4, 0, 0))
+        side_offset = 20
+        guard_right = wp().box(sg_width_2 + 12, sg_length, sg_height).translate((sg_width / 4, 0, 0))
 
         guard_left = wp().box(sg_width_2, sg_length - side_offset, sg_height).translate((-sg_width / 4, -side_offset / 2, 0))
 
@@ -424,17 +424,17 @@ def hand_chair():
         # return guard  # make_wall(side_guard_length, side_guard_width, side_guard_height, (0, 0, 0), (0, 0, 0))
 
     def make_top_base():
-        pts = [
-            (-tw2, tl2),  # top left
-            (tw2, tl2),  # top right
-            # (tw2, 0),  # mid right
-            # (tw2 + side_guard_width, 0),  # top right guard corner
-            # (tw2 + side_guard_width, -tl2),  # bottom right
-            (-tw2, -tl2),  # bottom left
-            (-tw2, tl2)  # top left
-        ]
+        # pts = [
+        #     (-tw2, tl2),  # top left
+        #     (tw2, tl2),  # top right
+        #     # (tw2, 0),  # mid right
+        #     # (tw2 + side_guard_width, 0),  # top right guard corner
+        #     # (tw2 + side_guard_width, -tl2),  # bottom right
+        #     (-tw2, -tl2),  # bottom left
+        #     (-tw2, tl2)  # top left
+        # ]
 
-        outline = wp().polyline(pts).close()
+        # outline = wp().polyline(pts).close()
 
         # outline.fillet(2)
         # circ = wp().circle(tw2).translate((0, tl2, 0))
@@ -527,8 +527,64 @@ def hand_chair():
         # top = top.faces("<Z").edges().fillet(0.5)
         # top = top.rotate((-1, 0, 0), (1, 0, 0), 10)
         top = top.translate((0, 0, height))
-        return top.translate((-5, 0, 0))
+        return top.translate((-5, 0, 0))   
+    
+    def make_trunk():
+        iso_external = IsoThread(
+            major_diameter=13.2 * MM,
+            pitch=1.8 * MM,
+            length=height * MM,
+            external=True,
+            end_finishes=("square", "square"),
+            hand="right"
+        )
+        print(f"root_radius: {iso_external.root_radius}")
+        external_core = Cylinder(
+            iso_external.root_radius,
+            height * MM,
+            align=(Align.CENTER, Align.CENTER, Align.MIN),
+        )
+        iso_external_screw = iso_external.fuse(external_core)
 
+        return iso_external_screw
+    
+    def make_base_post():
+        iso_internal = IsoThread(
+            major_diameter=14 * MM,
+            pitch=1.8 * MM,
+            length=height * MM / 1.5,
+            external=False,
+            end_finishes=("chamfer", "fade"),
+            hand="right",
+        )
+        with BuildPart() as iso_internal_shaft:
+            with BuildSketch():
+                Circle(iso_internal.major_diameter / 2 + 3)
+                Circle(iso_internal.major_diameter / 2, mode=Mode.SUBTRACT)
+            extrude(amount=iso_internal.length)
+            
+        return iso_internal.fuse(iso_internal_shaft.part)
+        
+    def make_nut():
+        iso_internal = IsoThread(
+            major_diameter=14 * MM,
+            pitch=1.8 * MM,
+            length=8 * MM,
+            external=False,
+            end_finishes=("chamfer", "fade"),
+            hand="right",
+        )
+        
+        with BuildPart() as iso_internal_nut:
+            with BuildSketch():
+                RegularPolygon(iso_internal.major_diameter * 0.75, 6)
+                Circle(iso_internal.major_diameter / 2, mode=Mode.SUBTRACT)
+            extrude(amount=iso_internal.length)
+
+        nut = iso_internal.fuse(iso_internal_nut.part)
+        
+        return nut
+    
     def make_bottom_holder():
         front_width = top_width / 2
         base_width = top_width
@@ -537,22 +593,41 @@ def hand_chair():
         bw2 = base_width / 2
         fw2 = (base_width - front_width) / 2
 
-        pts = [
-            (-fw2, bl2),  # top left
-            (fw2, bl2),  # top right
-            (bw2, -bl2),  # mid right
-            (-bw2, -bl2),  # bottom left
-            (-fw2, bl2)  # top left
-        ]
+        # pts = [
+        #     (-fw2, bl2),  # top left
+        #     (fw2, bl2),  # top right
+        #     (bw2, -bl2),  # mid right
+        #     (-bw2, -bl2),  # bottom left
+        #     (-fw2, bl2)  # top left
+        # ]
 
+        bottom = wp().circle(25).extrude(3)
+        x = 0
+        y = 0
+        offset = 15
+        for i in range(3):
+            outline = wp().rect(30, 60)
+            angle = math.pi * 2 / 3 * i
+            x = math.sin(angle) * offset
+            y = math.cos(angle) * offset
+           
+            plate = outline.extrude(3)
+            plate = plate.faces().edges("|Z").fillet(10)
+            plate =  plate.rotate((0, 0, 1), (0, 0, -1), i * 120)
+            plate = plate.translate((x, y, 0))
+  
+            bottom = bottom.union(plate)
+            
         # outline = wp().polyline(pts).close()
-        outline = wp().circle(30).translate((-15, 0))
-        base = outline.extrude(3)
-        outline = wp().rect(60, 60)
-        base = base.union(outline.extrude(3))
-        outline = wp().circle(30).translate((15, 0))
-        base = base.union(outline.extrude(3))
+        # outline = wp().rect(40, 40)
+     
+        # base = outline.extrude(3).translate((-15, 0, 0))
+        # outline = wp().rect(30, 60)
+        # base = base.union(outline.extrude(3))
+        # outline = wp().circle(30)
+        # base = base.union(outline.extrude(3).translate((15, 0, 0)))
 
+        base = bottom
         # base = base.faces().edges("|Z").fillet(10)
 
         base = base.faces().edges("<Z").chamfer(1)
@@ -561,8 +636,38 @@ def hand_chair():
 
         # trunk = wp().box(15, 30, height).translate((0, -15, height / 2))
         # trunk = trunk.faces().edges("|Z").fillet(5)
-        trunk = wp().cylinder(height, 7).translate((0, 0, height / 2))
+        trunk_new = make_trunk()
+                # external_core = Cylinder(
+        #     iso_external.root_radius,
+        #     iso_external.height * MM,
+        #     align=(Align.CENTER, Align.CENTER, Align.MIN),
+        # )
+        # iso_external_screw = iso_external.fuse(external_core)
+        core_radius = 5.6
+        trunk = cq.Solid.makeCylinder(core_radius, height)  # trunk = cq.Solid.makeCylinder(6.188101183952089, height)
+        # cq_obj = convert_shapes_to_cq(trunk_new)
+        trunk.wrapped = trunk_new.solid().wrapped  # cq.Solid.makeCylinder(7, height).translate((0, 0, height / 2))
+        cyl = wp().cylinder(height, core_radius).translate((0, 0, height / 2 - 1)).faces("<Z").edges().chamfer(0.5)
+        trunk = cyl.union(trunk)
+        shaft = make_base_post()
+        nut = make_nut()
 
+        cq_shaft = cq.Solid.makeCylinder(13, height)
+        cq_shaft.wrapped = shaft.solid().wrapped
+        
+        
+        # cq_obj = None
+        # for obj in trunk_new.solids():
+        #     new_obj = wp().cylinder(10, 5)
+        #     if cq_obj is None:
+        #         cq_obj = new_obj
+        #         cq_obj.wrapped = obj.wrapped
+        #     else:
+        #         new_obj.wrapped = obj.wrapped
+        #         cq_obj = cq_obj.union(new_obj)
+                
+        # trunk = cq_obj
+        
         ball_join = (wp().sphere(socket_radius)
                      .union(wp().cylinder(30, 2.5).rotate((1, 0, 0), (-1, 0, 0), 90).translate((0, 0, -2)))
                      .union(wp().cylinder(30, 2.5).rotate((0, 1, 0), (0, -1, 0), 90).translate((0, 0, -2))))
@@ -570,15 +675,18 @@ def hand_chair():
         ball_join = ball_join.translate((0, 0, height))
         # return top.union(guard).union(palm_cup)
         # top = trunk.union(ball)
-        return base.union(trunk).union(ball_join)
+        ball_shaft = ball_join.union(trunk)
+        base = base.union(cq_shaft)
+        return base, ball_shaft, nut
 
     # guard_base = make_guard(side_guard_width, side_guard_length, side_guard_height * 2)
     # guard_cutter = make_guard(side_guard_width - 4, side_guard_length + 10, side_guard_height * 2)
     # guard_cutter = guard_cutter.translate((0, 5, side_guard_height))
     # # guard_cutter = guard_cutter.faces("|Z").edges().fillet(2)
     # guard_base = guard_base.cut(guard_cutter)
-
-    return make_top_base(), make_bottom_holder()
+    top = make_top_base()
+    base, ball_shaft, nut = make_bottom_holder()
+    return top, base, ball_shaft, nut
 
 
 def puck_holder():
@@ -651,16 +759,24 @@ def usb_c_mount():
     return walls
 
 
-chair_top, chair_mount = hand_chair()
+chair_top, chair_mount, shaft, nut = hand_chair()
 
-cq.exporters.export(threaded_cylinder(), "./threaded_cylinder.stl")
+show(
+    chair_top.translate((0, 0, 15)), 
+    shaft,
+    nut.translate((20, 20, 0)),
+    chair_mount.translate((0, 0, -40))
+)
+
+# cq.exporters.export(threaded_cylinder(), "./threaded_cylinder.stl")
 # show(chair_top.translate((0, 0, 15)).union(chair_mount))
 # show(chair_mount)
-
-# cq.exporters.export(chair_top, "./hand_chair_top.stl")
+export_stl(nut, "./nut.stl")
+cq.exporters.export(shaft, "./shaft.stl")
 cq.exporters.export(chair_mount, "./hand_chair_mount.stl")
 cq.exporters.export(chair_top, "./hand_chair_top.stl")
 
 cq.exporters.export(chair_mount, "./hand_chair_mount.step")
 cq.exporters.export(chair_top, "./hand_chair_top.step")
-# cq.exporters.export(guard, "./guard.stl")
+
+cq.exporters.export(shaft, "./shaft.step")
